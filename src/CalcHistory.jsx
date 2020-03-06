0b9766ec695430febfc24b6041981a8a5f6c894d
keyvan.fa89@live.com <keyvan.fa89@live.com>
2020-03-06 20:32:58 +0330

    MySimpleCalc

---
+++ src/CalcHistory.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
+
+export default class CalcHistory extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+
+    state = { history: this.props.history }
+
+    removeHistory = (index) => {
+        let currentHistory = this.state.history;        
+        currentHistory.splice(index, 1);
+        this.setState({ history: currentHistory });
+        
+    }
+
+    render() {
+        return (
+            <div className="row">
+                {
+                    this.props.history.map(
+                        (item, index) => {
+                            return (
+                                <div className="col-12 anim-slide-up remove-fade-out" key={index} id={"history-item-" + index}>
+                                    <div class="input-group mb-3">
+                                        <div class="input-group-prepend" onClick={() => this.removeHistory(index)}>
+                                            <span class="input-group-text"><i className="cursor-hand far fa-trash-alt text-danger"></i></span>
+                                        </div>
+                                        <input type="text" value={item} readOnly="readonly" class="form-control" aria-label="Amount (to the nearest dollar)" />
+                                    </div>
+                                </div>
+                            )
+                        })
+                }
+            </div>
+        )
+    }
+}
---
+++ src/MyCalc.jsx
@@ -0,0 +1,86 @@
+import React, { Component } from 'react'
+import CalcHistory from './CalcHistory';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+export default class MyCalc extends Component {
+
+    state = { result: 0, number1: 0, number2: 0, history: [] };
+
+    addHistory = (operand, res) => {
+        let currentHistory = this.state.history;
+        this.setState({ history: currentHistory });
+        currentHistory.push(`${this.state.number1} ${operand} ${this.state.number2} = ${res}`);
+    }
+
+    calc = (operand) => {
+        let number1 = Number(this.state.number1);
+        let number2 = Number(this.state.number2);
+        let res = 0;
+        if (operand === '+') {
+            res = number1 + number2;
+        }
+        else if (operand === '-') {
+            res = number1 - number2;
+        }
+        else {
+
+        }
+        this.setState({ result: res });
+        this.addHistory(operand, res);
+    }
+
+
+
+    onChangeValues = (e) => {
+        this.setState({ [e.target.name]: [e.target.value] });
+    }
+
+    render() {
+        return (
+            <>
+                <div className="card">
+                    <div className="card-header bg-primary text-white">My simple calculator</div>
+                    <div className="card-body">
+                        <div className="row">
+                            <div className="col-6">
+                                <div className="form-group">
+                                    <label for="number1">Number 1</label>
+                                    <input type="number" 
+                                           id="number1"
+                                           name="number1" 
+                                           className="form-control"
+                                           value={this.state.number1} 
+                                           onChange={this.onChangeValues} />
+                                </div>
+                                <div className="form-group">
+                                    <label for="number2">Number 2</label>
+                                    <input type="number"
+                                           id="number2"
+                                           name="number2"
+                                           className="form-control"
+                                           value={this.state.number2}
+                                           onChange={this.onChangeValues} />
+                                </div>
+                                <div className="btn-group">
+                                    <button className="btn btn-success" 
+                                            onClick={() => this.calc('+')}>+
+                                            </button>
+                                    <button className="btn btn-danger" 
+                                            onClick={() => this.calc('-')}>-
+                                            </button>
+                                    <input  type="text" 
+                                            value={this.state.result} 
+                                            readOnly="readonly"
+                                            className="form-control" />
+                                </div>
+                            </div>
+                            <div className="col-3">
+                                <CalcHistory history={this.state.history} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
+}
