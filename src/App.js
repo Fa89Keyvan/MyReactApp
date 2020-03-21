d385bc156a7b8a9649f9e3101ae23a62a9b80a22
keyvan F.A <keyvan.fa89@live.com>
2020-03-21 13:40:22 +0430

    add router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import './App.css';
 import MyCalc from './MyCalc';
+import Home from './Home';
+import 'bootstrap/dist/css/bootstrap.css';
+import {
+  HashRouter as Router,
+  NavLink,
+  Switch
+} from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <MyCalc/>
-    </>
+    <Router>
+      <div>
+        <div className="row">
+          <div className="col-sm-12">
+            <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <NavLink exact to="/" className="nav-link">Home</NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink exact to="/Calculator" className="nav-link">Calculator</NavLink>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+
+
+        <div className="row">
+          <div className="col-12">
+            <Switch>
+              <Router exact path="/"><Home /></Router>
+              <Router exact path="/Calculator"><MyCalc /></Router>
+            </Switch>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
