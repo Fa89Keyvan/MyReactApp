3bff85328ed50184937f21403c12ce6e979a2d2d
keyvan F.A <keyvan.fa89@live.com>
2020-04-03 15:39:51 +0430

    pagination dynamic

--- src/Components/Reqres/Users.jsx
+++ src/Components/Reqres/Users.jsx
@@ -5,12 +5,9 @@ import { baseUrl } from './ReqresSettings';
 export default class Users extends Component {
 
     usersRoute = "/Reqres/UsersList"
-    state = { usersList: [], showingItems: 0, page: 1, total: 0 };
+    state = { usersList: [], showingItems: 0, page: 1, total: 0, pageSize: 5, lastPage: 0 };
     constructor(props) {
         super(props);
-
-        this.loadUsersList = this.loadUsersList.bind(this);
-        this.changePage = this.changePage.bind(this);
     }
 
     // getQuery = () => {
@@ -18,45 +15,59 @@ export default class Users extends Component {
     //     return new URLSearchParams(hash.substring(hash.indexOf("?")));
     // }
 
-    componentDidMount = () => { console.log("DidMount"); this.loadUsersList(); }
+    componentDidMount = () => this.loadUsersList();
 
-    changePage = (p) => this.setState({ ...this.state, page: p }, () => this.loadUsersList());
-
-    
+    changePage = (p) => {
+        
+        let { lastPage } = this.state;
+ 
+        if(p < 1){
+            p = 1;
+        } else if(p > lastPage){
+            p = lastPage;
+        }
+        
+        this.setState({ ...this.state, page: p }, () => this.loadUsersList());
+    }
 
     loadUsersList = () => {
-        const { page } = this.state;
+        const { page,pageSize } = this.state;
         console.log("loding page:", page);
-        fetch(baseUrl + `/users?page=${this.state.page}`)
+        fetch(baseUrl + `/users?page=${page}&per_page=${pageSize}`)
             .then(response => response.json())
             .then((response) => {
 
                 let usersList = response.data;
                 let showingItems = usersList.length;
-
+                let total = response.total;
+                let lastPage = Math.ceil(total / pageSize);
+                console.log("LastPage:",lastPage);
                 this.setState(
                     {
                         ...this.state,
+                        lastPage: lastPage,
                         usersList: usersList,
-                        total: response.total,
+                        total: total,
                         showingItems: showingItems
                     });
             });
     }
 
 
-    // renderPagination = () =>{
-
-    //     let paginationItems;
-    //     let { total,  }
-    //     let pagesCount = total / 
-
-
-    //     for (let i = 0; i < ; i++) {
-
-    //     }
-
-    // }
+    renderPagination = () =>{
+        let paginationItems = [];
+        let { lastPage  } = this.state;
+        
+        for (let i = 0; i < lastPage; i++) {
+            paginationItems.push( 
+                <li className="page-item" key={i}>
+                    <button className="page-link" onClick={() => this.changePage(i+1)}>{i+1}</button>
+                </li>
+            )
+        }
+        
+        return paginationItems;
+    }
 
     render() {
         console.log("calling render");
@@ -99,14 +110,15 @@ export default class Users extends Component {
                                             </td>
                                             <td>
                                                 <ul className="pagination pagination-sm">
-                                                    {/* <li className="page-item"><a className="page-link" href="#">Previous</a></li> */}
                                                     <li className="page-item">
-                                                        <button className="page-link" onClick={() => this.changePage(1)}>1</button>
+                                                        <button className="page-link" onClick={() => this.changePage(this.state.page - 1)}>Previous</button>
                                                     </li>
+                                                    {
+                                                        this.renderPagination().map((elem)=>elem)
+                                                    }
                                                     <li className="page-item">
-                                                        <button className="page-link" onClick={() => this.changePage(2)}>2</button>
+                                                        <button className="page-link" onClick={() => this.changePage(this.state.page + 1)}>Next</button>
                                                     </li>
-                                                    {/* <li className="page-item"><a className="page-link" href="#">Next</a></li> */}
                                                 </ul>
                                             </td>
                                         </tr>
